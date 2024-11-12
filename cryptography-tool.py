0f15506f36de7e58df5696c58853834dc10d5833
zxk-afz <zakaryfofana@proton.me>
2024-11-11 22:46:09 -0500

    feat: decrypt AES

--- cryptography-tool.py
+++ cryptography-tool.py
@@ -13,9 +13,11 @@ def encrypt_AES(input_file, output_file, key):
     with open(input_file, 'rb') as file:
         data = file.read()
 
-    # generate random iv
+    # Generate random iv
     iv = os.urandom(16)
 
+    # Padder ensure that data is encrypted in blocks (e.g., 13 bytes string replaced with a 16 bytes string) 
+    # (REMOVED WHEN DECRYPTED)
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(data) + padder.finalize()
 
@@ -25,3 +27,20 @@ def encrypt_AES(input_file, output_file, key):
 
     with open(output_file, 'wb') as file:
         file.write(iv + encrypted_data)
+
+# AES decryption
+def decrypt_AES(input_file, output_file, key):
+    with open(input_file, 'rb') as file:
+        iv = file.read(16)
+        encrypted_data = file.read()
+    
+    cipher = Cipher(algorithms.AES(key), modes.cbc(iv), backend=default_backend)
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(encrypted_data) = decryptor.finalize() 
+
+    # REMOVE THE PADDING
+    unpadder = padding.PKCS7(128).unpadder()
+    data = unpadder.update(decrypted_data) + unpadder.finalize()
+
+    with open(output_file, 'wb') as file:
+        file.write(data)
